c1aabfe5bd983e16339fb9c8d174345e299eb0b2
Gabor Szoboszlai <itgipsy@itgipsy.com>
2017-11-01 11:02:37 +0000

    Fix jquery load

--- exowellmd/functions.php
+++ exowellmd/functions.php
@@ -8,7 +8,7 @@ function theme_enqueue_scripts() {
 	wp_enqueue_style( 'Bootstrap_css', get_template_directory_uri() . '/css/bootstrap.min.css' );
 	wp_enqueue_style( 'MDB', get_template_directory_uri() . '/css/mdb.min.css' );
 	wp_enqueue_style( 'Style', get_template_directory_uri() . '/style.css' );
-	wp_enqueue_script( 'jQuery', get_template_directory_uri() . '/js/jquery-3.1.1.min.js', array(), '2.2.3', true );
+	wp_enqueue_script( 'jQuery', get_template_directory_uri() . '/js/jquery-3.2.1.min.js', array(), '3.2.1', true );
 	wp_enqueue_script( 'Tether', get_template_directory_uri() . '/js/popper.min.js', array(), '1.0.0', true );
 	wp_enqueue_script( 'Bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', array(), '1.0.0', true );
 	wp_enqueue_script( 'MDB', get_template_directory_uri() . '/js/mdb.min.js', array(), '1.0.0', true );
