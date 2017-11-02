7cf23acea7639df6394f2221eb0b95fe74445819
Gabor Szoboszlai <itgipsy@itgipsy.com>
2017-11-02 14:52:24 -0500

    Added searchform

---
+++ exowellmd/searchform.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Template for displaying search forms in ExoWellMD
+ */
+?>
+<form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+  <label for="s" class="sr-only"><?php _e( 'Search' ); ?></label>
+  <input type="text" class="form-control" name="s" id="s" 
+    placeholder="<?php esc_attr_e( 'Search' ); ?>">
+<!--  <input type="submit" class="btn btn-sm themecolor" id="searchsubmit" value="<?php esc_attr_e( 'Search' ); ?>"> -->
+</form>
