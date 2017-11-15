dd56a62dc2348106e4232cca39e91cc5cd13b2f2
Gabor Szoboszlai <itgipsy@itgipsy.com>
2017-11-15 10:15:37 -0600

    Basic post format

--- exowellmd/template-parts/content.php
+++ exowellmd/template-parts/content.php
@@ -1,28 +1,32 @@
 <!--Post-->
-<div class="post-wrapper">
-	<!--Post data-->
-	<a href="<?php echo get_permalink() ?>"><h1 class="h1-responsive"><?php the_title(); ?></h1></a>
-	<h5>Written by <a href=""><?php the_author(); ?></a>, <?php echo get_the_date(); ?></h5>
-	<br>
+<div class="card post-wrapper">
 	<!--Featured image -->
-	<div class="view overlay hm-white-light z-depth-1-half">
-		<?php the_post_thumbnail( 'full', array( 'class'=> 'img-fluid z-depth-2')); ?>
-		<div class="mask">
+	<?php if ( has_post_thumbnail() ) : ?>
+		<div class="view overlay post-image-overlay">
+			<?php the_post_thumbnail('large', array('class' => 'card-img-top img-fluid')); ?>
+    		<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" class="post-image"><div class="mask"></div></a>
+		</div>
+	<?php endif; ?>
+	<!--Post data-->
+	<div class="card-body post-block">
+		<a href="<?php echo get_permalink() ?>"><h4 class="card-title post-title"><?php the_title(); ?></h4></a>
+		<div class="card-text post-meta">
+			Posted by <?php echo get_the_author_link(); ?> on <?php echo get_the_date(); ?>
+			<hr>
+		</div>
+		<div class="card-text post-content">
+			<?php
+			if (is_single()){
+				the_content();
+			} else {
+				the_content('', false);
+			?>
+			<!--"Read more" button-->
+			<a href="<?php echo get_permalink() ?>"><button class="btn themecolor">Read more</button></a>
+			<?php
+			}
+			?>
 		</div>
 	</div>
-	<br>
-	<?php
-	if (is_single()){
-		the_content();
-	}
-	else {
-		the_content('', false);
-	?>
-		<!--"Read more" button-->
-		<a href="<?php echo get_permalink() ?>"><button class="btn themecolor">Read more</button></a>
-	<?php
-	}
-	?>
 </div>
 <!--/.Post-->
-<hr>
