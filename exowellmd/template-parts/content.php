df47328c2fe4e45c40d86c3414a9d06cfeb16a65
Gabor Szoboszlai <itgipsy@itgipsy.com>
2017-11-15 10:57:46 -0600

    Post meta pills

--- exowellmd/template-parts/content.php
+++ exowellmd/template-parts/content.php
@@ -11,8 +11,28 @@
 	<div class="card-body post-block">
 		<a href="<?php echo get_permalink() ?>"><h4 class="card-title post-title"><?php the_title(); ?></h4></a>
 		<div class="card-text post-meta">
-			Posted by <?php echo get_the_author_link(); ?> on <?php echo get_the_date(); ?>
-			<hr>
+			<span class="badge badge-pill post-author themecolor">
+				<i class="fa fa-user-circle" aria-hidden="true"></i>
+				<span class="sr-only">Author:</span>
+				<?php echo get_the_author_link(); ?>
+			</span>
+			<span class="badge badge-pill post-date themecolor">
+				<i class="fa fa-calendar" aria-hidden="true"></i>
+				<span class="sr-only">Posted on:</span>
+				<?php echo get_the_date(); ?>
+			</span>
+			<?php
+				$categories = array();
+				foreach ( (get_the_category()) as $category ){
+					$categories[] = '<a href="' . get_category_link($category->term_id) . '">' . $category->cat_name . '</a>';
+				}
+				$cat_str = implode(', ', $categories);
+			?>
+			<span class="badge badge-pill post-categories themecolor">
+				<i class="fa fa-folder-o" aria-hidden="true"></i>
+				<span class="sr-only">Post categories:</span>
+				<?php echo $cat_str; ?>
+			</span>
 		</div>
 		<div class="card-text post-content">
 			<?php
