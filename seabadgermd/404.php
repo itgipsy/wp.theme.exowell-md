d89733473d2f8988389beeeffe19e0de5f8f6d55
Gabor Szoboszlai <itgipsy@itgipsy.com>
2017-11-21 09:06:21 -0600

    Added 404 page

---
+++ seabadgermd/404.php
@@ -0,0 +1,27 @@
+<?php get_header(); ?>
+<main>
+<span id="top" style="display:none"></span>
+<!--Main layout-->
+<div class="container">
+	<div class="row">
+		<!--Main column-->
+		<div class="col-xs-12 col-md-8">
+			<div class="row">
+				<div class="card col-xs-12 col-md-8 offset-md-2">
+					<div class="card-body">
+						<h4 class="card-title"><?= __('404 - Not found', 'seabadgermd'); ?></h4>
+						<p class="card-text">Sorry, the content you are looking for is not available. Best I can offer you is this search bar. 
+						Please feel free to use it and hope you will find what you are looking for.</p>
+						<p class="card-text"><?php get_search_form(); ?></p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!--Sidebar-->
+		<?php get_sidebar(); ?>
+		<!--/Sidebar-->	
+	</div>
+</div>
+<!--/Main layout-->
+</main>
+<?php get_footer(); ?>
