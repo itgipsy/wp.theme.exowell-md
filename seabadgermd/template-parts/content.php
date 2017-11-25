5f97677addfeea8088804b822992095252921fe3
Gabor Szoboszlai <itgipsy@itgipsy.com>
2017-11-25 11:42:58 -0600

    Remove comma between tags

--- seabadgermd/template-parts/content.php
+++ seabadgermd/template-parts/content.php
@@ -71,7 +71,7 @@
 			<?php
 				if (has_tag()) {
 					echo '<hr><span class="text-muted tagline">' .  __('Tagged with ', 'seabadgermd');
-					echo get_the_tag_list('', ', ');
+					echo get_the_tag_list('', ' ');
 					echo '</span>';
 				}
 			?>
