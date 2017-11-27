0e91623b46ca6327cf5b44315199958058006d69
Gabor Szoboszlai <itgipsy@itgipsy.com>
2017-11-27 07:55:27 -0600

    Refactor Hero into template-part

--- seabadgermd/header.php
+++ seabadgermd/header.php
@@ -10,29 +10,7 @@
 </head>
 <body <?php body_class(); ?>>
 	<header class="<?php echo $header_class ?>">
-		<?php get_template_part( 'template-parts/navbar' ); ?>
-<!-- Hero -->
-<?php
-	if (get_theme_mod('seabadgermd_hero_show', false)) {
-		$hero_config = array(
-			'title' => get_theme_mod('seabadgermd_hero_title', ''),
-			'title_url' => get_theme_mod('seabadgermd_hero_title_url', ''),
-			'description' => get_theme_mod('seabadgermd_hero_description', ''),
-			'button' => array(
-				'text' => get_theme_mod('seabadgermd_hero_button_text', ''),
-				'href' => get_theme_mod('seabadgermd_hero_button_href', ''),
-				'color' => get_theme_mod('seabadgermd_hero_button_color', ''),
-				'bgcolor' => get_theme_mod('seabadgermd_hero_button_bgcolor', '')
-			),
-			'color' => get_theme_mod('seabadgermd_hero_color', ''),
-			'bgcolor' => get_theme_mod('seabadgermd_hero_bgcolor', ''),
-			'image' => get_theme_mod('seabadgermd_hero_image', ''),
-			'position' => get_theme_mod('seabadgermd_hero_position', ''),
-			'logo' => get_theme_mod('seabadgermd_hero_logo', '')
-		);
-		displayHero($hero_config);
-	}
-?>
-<!--/Hero -->
-<?php get_template_part( 'template-parts/breadcrumb' ); ?>
-</header>
+		<?php get_template_part('template-parts/navbar'); ?>
+		<?php get_template_part('template-parts/hero'); ?>
+		<?php get_template_part('template-parts/breadcrumb'); ?>
+	</header>
---
+++ seabadgermd/template-parts/hero.php
@@ -0,0 +1,93 @@
+<?php
+/** 
+* Displays a Hero / jumbotron based on the configuration
+*/
+if (!get_theme_mod('seabadgermd_hero_show', false)) {
+	return;
+}
+
+$config = array(
+	'title' => get_theme_mod('seabadgermd_hero_title', ''),
+	'title_url' => get_theme_mod('seabadgermd_hero_title_url', ''),
+	'description' => get_theme_mod('seabadgermd_hero_description', ''),
+	'button' => array(
+		'text' => get_theme_mod('seabadgermd_hero_button_text', ''),
+		'href' => get_theme_mod('seabadgermd_hero_button_href', ''),
+		'color' => get_theme_mod('seabadgermd_hero_button_color', ''),
+		'bgcolor' => get_theme_mod('seabadgermd_hero_button_bgcolor', '')
+	),
+	'color' => get_theme_mod('seabadgermd_hero_color', ''),
+	'bgcolor' => get_theme_mod('seabadgermd_hero_bgcolor', ''),
+	'image' => get_theme_mod('seabadgermd_hero_image', ''),
+	'position' => get_theme_mod('seabadgermd_hero_position', ''),
+	'logo' => get_theme_mod('seabadgermd_hero_logo', '')
+);
+
+$hero_style="";
+if ($config['bgcolor']) $hero_style .= 'background-color:' . $config['bgcolor'] . ';';
+if ($config['color']) $hero_style .= 'color:' . $config['color'] . ';';
+if ($config['image']) {
+	$hero_style .= 'background-image: url(\'' . $config['image'] . '\');';
+	$hero_style .= 'background-size: cover;';
+}
+if ($config['logo']) {
+	$offset_right = '5';
+	$offset_center = '2';
+} else {
+	$offset_right = '7';
+	$offset_center = '4';
+}
+switch($config['position']) {
+	case 'right': $arrangeclass = 'col-xs-12 col-md-5 offset-md-' . $offset_right;
+		break;
+	case 'center': $arrangeclass = 'col-xs-12 col-md-5 offset-md-' . $offset_center;
+		break;
+	case 'left': $arrangeclass = 'col-xs-12 col-md-5';
+		break;
+	default: //full
+		$arrangeclass = 'col-xs-12';
+		if ($config['logo']) {
+			$arrangeclass .= ' col-md-10';
+		}
+}
+?>
+<div class="container hero">
+	<div class="jumbotron row" style="<?php echo $hero_style ?>">
+<?php if ($config['logo']): ?>
+	<div class="col-xs-12 col-md-2 text-center hero-logo">
+		<a href="/"><img class="logo" aria-label="Logo to homepage" src="<?php echo $config['logo'] ?>"></a>
+	</div>
+<?php endif; ?>
+		<div class="<?php echo $arrangeclass ?>">
+<?php if ($config['title']) : ?>
+	<h1 class="display-3 hero-title">
+		<?php
+			if ($config['title_url']) {
+				$style = '';
+				if ($config['color']) {
+					$style = sprintf('style="color: %s!important"', $config['color']);
+				}
+				printf('<a href="%s" %s>%s</a>', $config['title_url'], $style, $config['title']);
+			} else {
+				echo $config['title'];
+			}
+		?>
+	</h1>
+<?php endif; ?>
+<?php if ($config['description']) : ?>
+	<p class="lead hero-description"><?php echo $config['description'] ?></p>
+<?php endif; ?>
+<?php if (is_array($config['button']) && $config['button']['text'] && $config['button']['href']) {
+	$button = $config['button'];
+	$btn_style;
+	if ($button['bgcolor']) $btn_style .= 'background-color: ' . $button['bgcolor'] . ';';
+	if ($button['color']) $btn_style .= 'color: ' . $button['color'] . ';';
+?>
+	<p class="lead hero-button-area">
+		<a class="btn btn-lg" style="<?php echo $btn_style ?>" href="<?php echo $button['href'] ?>" 
+			role="button"><?php echo $button['text'] ?></a>
+	</p>
+<?php } ?>
+		</div>
+	</div>
+</div>
