1bb74b9ecf24ce8afd0affa110317330343d3cce
Gabor Szoboszlai <itgipsy@itgipsy.com>
2017-11-29 07:04:26 -0600

    Add theme support custom-background

--- seabadgermd/functions.php
+++ seabadgermd/functions.php
@@ -53,6 +53,8 @@ function seabadgermd_setup() {
 	// Add featured image support
 	add_theme_support('post-thumbnails');
 	add_image_size('main-full', 1078, 516, false); // main post image in full width
+	// Allow custom background
+	add_theme_support('custom-background');
 }
 add_action('after_setup_theme', 'seabadgermd_setup');
 
