7975f761fc70c3074ebf9ce676110d0936188c87
Gabor Szoboszlai <itgipsy@itgipsy.com>
2017-11-30 07:31:27 -0600

    Fix sticky mark, only show when on homepage

--- seabadgermd/template-parts/content.php
+++ seabadgermd/template-parts/content.php
@@ -7,12 +7,15 @@
 		</a>
 	<?php endif; ?>
 	<!--Post data-->
+	<?php
+		$is_sticky = is_sticky() && is_home() && !is_paged();
+	?>
 	<div class="card-body post-block">
 		<a href="<?php echo get_permalink() ?>">
-			<h4 class="card-title post-title <?php echo (is_sticky()) ? 'sticky' : ''; ?>">
+			<h4 class="card-title post-title <?php echo ($is_sticky) ? 'sticky' : ''; ?>">
 				<?php the_title(); ?>
 				<?php
-					if (is_sticky()) echo '<i class="fa fa-anchor sticky_icon"></i>';
+					if ($is_sticky) echo '<i class="fa fa-anchor sticky_icon"></i>';
 				?>
 			</h4>
 		</a>
