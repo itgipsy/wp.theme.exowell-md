2a71de933f4e3facb70e6e24dd03f47c31bfdda0
Gabor Szoboszlai <itgipsy@itgipsy.com>
2017-12-13 16:11:29 +0000

    Fixing code style to match WordPress-Core CS rules

--- seabadgermd/404.php
+++ seabadgermd/404.php
@@ -8,10 +8,10 @@
 		<div class="col-xs-12 col-md-8">
 			<div class="row">
 				<div class="card col-xs-12 col-md-8 offset-md-2">
-					<img class="img-fluid" src="<?php echo SBMD_THEME_DIR_URI; ?>/img/404.png">
+					<img class="img-fluid" src="<?php esc_attr_e( SBMD_THEME_DIR_URI ); ?>/img/404.png">
 					<div class="card-body">
-						<h4 class="card-title"><?php _e( '404 - Not found', 'seabadgermd' ); ?></h4>
-						<p class="card-text"><?php _e( 'Sorry, the content you are looking for is not available.
+						<h4 class="card-title"><?php esc_html_e( '404 - Not found', 'seabadgermd' ); ?></h4>
+						<p class="card-text"><?php esc_html_e( 'Sorry, the content you are looking for is not available.
 							Best I can offer you is this search bar. 
 							Please feel free to use it and hope you will find what you are looking for.', 'seabadgermd' ); ?></p>
 						<p class="card-text"><?php get_search_form(); ?></p>
--- seabadgermd/footer.php
+++ seabadgermd/footer.php
@@ -3,36 +3,31 @@
 	<div class="row">
 		<div class="footer-widget col-md-6 col-lg-3">
 			<?php
-				if (is_active_sidebar('footer')) {
-					dynamic_sidebar('footer');
-				}
+			if ( is_active_sidebar( 'footer' ) ) {
+				dynamic_sidebar( 'footer' );
+			}
 			?>
 		</div>
 	
 		<!--Footer menu -->
-		<?php
-			if (has_nav_menu('footer')) {
-		?>
+		<?php if ( has_nav_menu( 'footer' ) ) : ?>
 		<div class="footer-menu col-md-6 col-lg-5">
 			<?php
 			wp_nav_menu( array(
 				'menu' => 'footer',
 				'theme_location' => 'footer',
-				'depth' => 1
-		 		)
-		 	);
+				'depth' => 1,
+			));
 			?>
 		</div>
-		<?php
-			}
-		?>
+		<?php endif; ?>
 		<!--/Footer menu-->
 	
 		<!--Copyright-->
 		<div class="footer-copyright col-md-12 col-lg-4">
 			<?php
-				printf(__('Copyright &copy; %1$s <a href="%2$s" rel="nofollow">%3$s</a>', 'seabadgermd'),
-					date("Y"), get_site_url(), get_bloginfo('name'));
+			printf( __( 'Copyright &copy; %1$s <a href="%2$s" rel="nofollow">%3$s</a>', 'seabadgermd' ),
+			date( 'Y' ), get_site_url(), get_bloginfo( 'name' ) );
 			?>
 		</div>
 		<!--/Copyright-->
@@ -40,8 +35,8 @@
 	<div class="row footer-themeinfo">
 		<div class="col-12">
 		<?php $sbmd_theme = wp_get_theme(); ?>
-			Theme by <a href="<?php echo $sbmd_theme->get('AuthorURI'); ?>" rel="nofollow">
-			<?php echo $sbmd_theme->get('Author'); ?></a>
+			Theme by <a href="<?php echo $sbmd_theme->get( 'AuthorURI' ); ?>" rel="nofollow">
+			<?php echo $sbmd_theme->get( 'Author' ); ?></a>
 		</div>
 	</div>
 </footer>
