77800334f29588b905204a8698dc8323ea84c7af
Gabor Szoboszlai <szobinet@gmail.com>
2018-01-02 11:55:00 +0100

    Fix header color, added home link

--- seabadgermd/template-parts/hero.php
+++ seabadgermd/template-parts/hero.php
@@ -4,22 +4,27 @@
 */
 $header_image = get_header_image();
 $text_color = get_header_textcolor();
+if ( preg_match( '/^[0-9a-fA-F]{6}$/', $text_color ) ) {
+	$text_color = '#' . $text_color;
+}
 // only show header if both header image and text or logo are enabled
 if ( ! $header_image || ( 'blank' === $text_color && ! has_custom_logo() ) ) {
 	return;
 }
 
 $hero_style = '';
-$hero_style .= 'color:' . get_header_textcolor() . ';';
+$hero_style .= 'color:' . $text_color . ';';
 $hero_style .= 'background-image: url(\'' . $header_image . '\');';
 $hero_style .= 'background-size: cover;';
 
 ?>
 <div class="container hero">
 	<div class="jumbotron row" style="<?php echo $hero_style; ?>">
-		<div class="col-xs-12 col-md-10" style="color:#<?php echo esc_attr( $text_color ); ?>!important">
-			<h1 class="hero-title" style="color:#<?php echo esc_attr( $text_color ); ?>">
-				<?php bloginfo( 'name' ); ?>
+		<div class="col-xs-12 col-md-10" style="color:<?php echo esc_attr( $text_color ); ?>!important">
+			<h1 class="hero-title">
+				<a href="<?php echo esc_url( get_site_url() ); ?>" style="color:<?php echo esc_attr( $text_color ); ?>">
+					<?php bloginfo( 'name' ); ?>
+				</a>
 			</h1>
 			<p class="lead hero-description"><?php bloginfo( 'description' ); ?></p>
 		</div>
@@ -28,14 +33,14 @@ $hero_style .= 'background-size: cover;';
 		?>
 		<div class="col-xs-12 col-md-2 text-center hero-logo">
 			<?php
-				$custom_logo = get_custom_logo();
+			$custom_logo = get_custom_logo();
 			if ( preg_match( '/class=/', $custom_logo ) ) {
 				$custom_logo = str_replace( 'class="', 'class="img-fluid ', $custom_logo );
 			} else {
 				$custom_logo = str_replace( '<img', '<img class="img-fluid"', $custom_logo );
 			}
-				echo $custom_logo;
-				?>
+			echo $custom_logo;
+			?>
 		</div>
 		<?php
 		}
